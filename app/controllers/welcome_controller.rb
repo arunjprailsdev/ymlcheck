2afb3a9a85f7c0ecdaddf6c4188eb3e70a374056
jpcoderesearch <jpcoderesearch@gmail.com>
2018-06-29 21:20:06 +0530

    render json

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,26 +6,27 @@ class WelcomeController < ApplicationController
 
   	user = User.all
 
-  	user.each do |val|
-  		# abort(val["style_profile"].inspect)
-  		hash = { a: true, b: false, c: nil}
-	    uu = hash.slice(:c) # => { a: true, b: false}
-	    hash # => { a: true, b: false }
-	    abort(hash.inspect)
-  	end
-  	abort(user[1]["style_profile"].inspect)
-  	# config = YAML.load_file("#{Rails.root}/lib/test.yml")
+#   	user.each do |val|
+#   		# abort(val["style_profile"].inspect)
+#   		hash = { a: true, b: false, c: nil}
+# 	    uu = hash.slice(:c) # => { a: true, b: false}
+# 	    hash # => { a: true, b: false }
+# 	    abort(hash.inspect)
+#   	end
+#   	abort(user[1]["style_profile"].inspect)
+#   	# config = YAML.load_file("#{Rails.root}/lib/test.yml")
   	
 
-  	# config = YAML.load(File.read("#{Rails.root}/lib/test.yml"))
-  	# abort("#{Rails.root}/lib/test.yml".inspect)
+#   	# config = YAML.load(File.read("#{Rails.root}/lib/test.yml"))
+#   	# abort("#{Rails.root}/lib/test.yml".inspect)
 
-# ["lib/test.yml"].each do |path|
-      yml = YAML.load_file(Rails.root.join("lib/test.yml"))
-      abort(yml["attributes"].include?("date").inspect)
+# # ["lib/test.yml"].each do |path|
+#       yml = YAML.load_file(Rails.root.join("lib/test.yml"))
+#       abort(yml["attributes"].include?("date").inspect)
 
 
-  # abort (yml['attributes']['shipping_comment'].inspect)
+#   # abort (yml['attributes']['shipping_comment'].inspect)
+render json: user
 
 
   end
